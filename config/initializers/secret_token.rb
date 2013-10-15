c4fb561ed23cb3189d6b3eff2609124b395d91a6
Brett Corbly <bcorbly@gmail.com>
2013-10-15 17:10:09 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Pintrestring::Application.config.secret_key_base = '9a5888d04b01ebc278471b496b642bdc93ad6172ab031f166cf4f100000f0b84d7a9008e3629d2f337c423b122aa4bed0888b9cac413b389b9bf91a141fe56cb'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Pintrestring::Application.config.session_store :cookie_store, key: '_pintrestring_session'
